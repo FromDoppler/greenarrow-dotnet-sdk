20b5ce75367ebab486fe05480c0f69c68440af1b
Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2023-06-15 15:00:59 -0300

    refactor: add extension to deserialize json object

--- GreenArrow.Engine/Extensions/JsonExtensions.cs
+++ GreenArrow.Engine/Extensions/JsonExtensions.cs
@@ -27,5 +27,16 @@ namespace GreenArrow.Engine.Extensions
         {
             return JsonConvert.SerializeObject(model, Formatting.Indented, settings);
         }
+
+        /// <summary>
+        /// Deserialize a JSON string into an specific type of object
+        /// </summary>
+        /// <typeparam name="T">Type of the expected object</typeparam>
+        /// <param name="json">JSON string to deserialize</param>
+        /// <returns>A object instance of the especified type </returns>
+        public static T ToObject<T>(this string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json, settings);
+        }
     }
 }
