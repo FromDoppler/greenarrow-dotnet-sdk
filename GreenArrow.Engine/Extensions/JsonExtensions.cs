da5dcf1dd799a0fd7be9ac94735f0995f1d640f8
Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2023-06-15 13:52:54 -0300

    refactor: serialize object to json with snake case
    Green Arrow use snake case for the name of the model objects

---
+++ GreenArrow.Engine/Extensions/JsonExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace GreenArrow.Engine.Extensions
+{
+    /// <summary>
+    /// Extensions methods for JSON serialization and deserialization
+    /// </summary>
+    public static class JsonExtensions
+    {
+        private static readonly JsonSerializerSettings settings = new()
+        {
+            ContractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new SnakeCaseNamingStrategy(),
+            },
+            NullValueHandling = NullValueHandling.Ignore,
+        };
+
+        /// <summary>
+        /// Serialize object to a json string in format expected by Green Arrow APIs
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static string ToJson<T>(this T model)
+        {
+            return JsonConvert.SerializeObject(model, Formatting.Indented, settings);
+        }
+    }
+}
