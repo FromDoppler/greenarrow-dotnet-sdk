c24c587ea5c06434b2c729ae30c7e310fe1781a9
Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2023-06-15 13:51:57 -0300

    test: serialize object to json with snake case

---
+++ GreenArrow.Engine.Test/Extensions/JsonExtensionsTest.cs
@@ -0,0 +1,23 @@
+﻿using GreenArrow.Engine.Extensions;
+
+namespace GreenArrow.Engine.Test.Extensions
+{
+    public class JsonExtensionsTest
+    {
+        [Fact]
+        public void ToJson_should_serialize_in_snake_case()
+        {
+            // Arrange
+            var objectToSerialize = new
+            {
+                ThisIsAProperty = "this is a property",
+            };
+
+            // Act
+            var json = objectToSerialize.ToJson();
+
+            // Assert
+            Assert.Contains("this_is_a_property", json);
+        }
+    }
+}
