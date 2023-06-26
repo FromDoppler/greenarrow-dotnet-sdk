5001f34a93f6694813a7e07a932323a3b873ff35
Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2023-06-26 09:44:07 -0300

    refactor: avoid circular reference

--- GreenArrow.Engine/HttpSubmissionApi/HttpSubmissionResponse.cs
+++ GreenArrow.Engine/HttpSubmissionApi/HttpSubmissionResponse.cs
@@ -3,33 +3,13 @@
 namespace GreenArrow.Engine.HttpSubmissionApi
 {
     /// <summary>
-    /// GreenArrow Response
+    /// GreenArrow Response for single or multiple delivery request
     /// </summary>
-    public class HttpSubmissionResponse : IRestApiModel
+    public class HttpSubmissionResponse : HttpSubmissionSingleResponse, IRestApiModel
     {
-        /// <summary>
-        /// When request was succesful treated
-        /// </summary>
-        public bool Success { get; init; }
-        /// <summary>
-        /// Error when request was not accepted
-        /// </summary>
-        public string Error { get; init; }
-        /// <summary>
-        /// Unique identifier
-        /// </summary>
-        public string Id { get; init; }
-        /// <summary>
-        /// Number of attemps
-        /// </summary>
-        public int Attempted { get; init; }
-        /// <summary>
-        /// The single message id
-        /// </summary>
-        public string MessageId { get; init; }
         /// <summary>
         /// An array of responses for many messages
         /// </summary>
-        public ICollection<HttpSubmissionResponse> Messages { get; init; }
+        public ICollection<HttpSubmissionSingleResponse> Messages { get; init; }
     }
 }
---
+++ GreenArrow.Engine/HttpSubmissionApi/HttpSubmissionSingleResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace GreenArrow.Engine.HttpSubmissionApi
+{
+    /// <summary>
+    /// GreenArrow Response of single delivery request
+    /// </summary>
+    public class HttpSubmissionSingleResponse
+    {
+        /// <summary>
+        /// Number of attemps
+        /// </summary>
+        public int Attempted { get; init; }
+        /// <summary>
+        /// Error when request was not accepted
+        /// </summary>
+        public string Error { get; init; }
+        /// <summary>
+        /// Unique identifier
+        /// </summary>
+        public string Id { get; init; }
+        /// <summary>
+        /// The single message id
+        /// </summary>
+        public string MessageId { get; init; }
+        /// <summary>
+        /// When request was succesful treated
+        /// </summary>
+        public bool Success { get; init; }
+    }
+}
