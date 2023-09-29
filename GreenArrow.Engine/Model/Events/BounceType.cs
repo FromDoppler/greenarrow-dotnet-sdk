e730f557478fd78bf5b51bcb7c2d70b09d46ac91
Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2023-09-29 15:23:29 -0300

    fix: deserialize chat to enum value for bounce type

--- GreenArrow.Engine/Model/Events/BounceType.cs
+++ GreenArrow.Engine/Model/Events/BounceType.cs
@@ -1,21 +1,31 @@
-﻿namespace GreenArrow.Engine.Model.Events
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace GreenArrow.Engine.Model.Events
 {
     /// <summary>
     /// The type of bounce. h for hard, s for soft, and o for other.
     /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum BounceType
     {
         /// <summary>
         /// Bounce for message that not be retried
         /// </summary>
+        [EnumMember(Value = "h")]
         Hard = 'h',
+
         /// <summary>
         /// Bounce for message that can be retried
         /// </summary>
+        [EnumMember(Value = "s")]
         Soft = 's',
+
         /// <summary>
         /// Other type of bounce
         /// </summary>
+        [EnumMember(Value = "o")]
         Other = 'o',
     }
 }
