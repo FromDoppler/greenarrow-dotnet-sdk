f9437be65cc99db64fb861993d0f6d7a728a8bc9
blancfabian <mariofabianblanc@gmail.com>
2024-04-23 15:24:42 -0300

    feat: new send status endpoint implementation

---
+++ GreenArrow.Engine/SendStatusApi/SendStatusApiClient.cs
@@ -0,0 +1,79 @@
+﻿using GreenArrow.Engine.Extensions;
+using GreenArrow.Engine.RestApi;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace GreenArrow.Engine.SendStatusApi
+{
+    /// <summary>
+    /// Send Status API client implementation
+    /// </summary>
+    public class SendStatusApiClient : ISendStatusApi
+    {
+        private readonly GreenArrowEngineSettings _settings;
+        private readonly IHttpClientFactory _httpFactory;
+
+        private readonly string _endpoint;
+
+        /// <summary>
+        /// Initializes a new instance of Green Arrow Send Status API Client
+        /// </summary>
+        /// <param name="options">Green Arrow settings with API Url and Authorization Token</param>
+        /// <param name="httpFactory">HttpClienFactory for create HttpClient objects</param>
+        public SendStatusApiClient(
+            IOptions<GreenArrowEngineSettings> options,
+            IHttpClientFactory httpFactory)
+        {
+            _settings = options.Value;
+            _httpFactory = httpFactory;
+            _endpoint = GetEndPoint();
+        }
+
+        private HttpClient CreateHttpClient()
+        {
+            var client = _httpFactory.CreateClient();
+            return client;
+        }
+
+        private string GetEndPoint()
+        {
+            var baseUri = new Uri(_settings.ServerUri);
+            var endpointUri = new Uri(baseUri, _settings.SendStatusAPIEndpoint);
+            return endpointUri.ToString();
+        }
+
+        /// <inheritdoc />
+        public async Task<IRestApiResponse<SendStatusResponse>> PostAsync(SendStatusRequest request, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var jsonContent = new { send = new { status = request.Send.Status } }.ToJson();
+
+                var client = CreateHttpClient();
+
+                var authenticationString = $"{request.Username}:{request.Password}";
+                var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(authenticationString));
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64EncodedAuthenticationString);
+
+                var httpContent = new StringContent(jsonContent, encoding: default, mediaType: "application/json");
+                var httpResponse = await client.PostAsync(string.Format(_endpoint, request.Send.Sendid), httpContent, cancellationToken);
+
+                if (httpResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    var result = await httpResponse.Content.ReadAsStringAsync(cancellationToken);
+                    var content = result.ToObject<SendStatusResponse>();
+                    return new RestApiResponse<SendStatusResponse>(httpResponse.StatusCode, content);
+                }
+
+                return new RestApiResponse<SendStatusResponse>(httpResponse.StatusCode);
+
+            }
+            catch (Exception exception)
+            {
+                throw new RestApiException("Unexpected exception", exception);
+            }
+        }
+    }
+}
---
+++ GreenArrow.Engine/SendStatusApi/SendStatusRequest.cs
@@ -0,0 +1,29 @@
+﻿using GreenArrow.Engine.Model;
+using GreenArrow.Engine.RestApi;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace GreenArrow.Engine.SendStatusApi
+{
+    /// <summary>
+    /// Create a Send Status Request
+    /// </summary>
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy), ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class SendStatusRequest : IRestApiModel
+    {
+        /// <summary>
+        /// Username that is authorized to log in to GreenArrow Engine’s web interface.
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Password that is authorized to log in to GreenArrow Engine’s web interface.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// The Send information
+        /// </summary>
+        public Send Send { get; set; }
+    }
+}
